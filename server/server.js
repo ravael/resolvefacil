1356b1e6573353d2cd464dcc709497b17db2c29d
Raphael <raphael@localhost.localdomain>
2017-12-04 23:12:02 -0200

    package json in root

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database');
 
-http.createServer(app).listen(process.env.PORT, function() {
+    var porta = process.env.PORT || 3000;
+    
+
+http.createServer(app).listen(porta, function() {
     console.log('Servidor estutando na porta: ' + this.address().port);
 });
 
